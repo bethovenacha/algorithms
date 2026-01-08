977789c339bcca7ac6d6e14d7ee630d80b9b30c9
BethovenAcha <bmacha2019@gmail.com>
2026-01-08 08:55:19 +0800

    first commit

---
+++ Arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public static class Arrays
+    {
+        public static int[] TwoNumberSum(int[] array, int targetSum)
+        {
+
+            int[] result = new int[2];
+            HashSet<int> h = new HashSet<int>();
+
+            foreach (var num in array)
+            {
+                int dif = targetSum - num;
+                if (h.Contains(dif))
+                {
+                    return new int[] { dif, num };
+                }
+                h.Add(num);
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using DataStructuresAndAlgorithms;
+
+int[] input = new[] { 4, 6, 1, -3 };
+int target = 3;
+var result  = Arrays.TwoNumberSum(input, target);
+Console.WriteLine($"[{string.Join(", ", result)}]");
+
