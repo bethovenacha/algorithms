73ee3e5fdfd6020f9427686915ed394da5c03ef5
BethovenAcha <bmacha2019@gmail.com>
2026-01-08 10:44:09 +0800

    commiting sorted squares of arrays

--- Arrays.cs
+++ Arrays.cs
@@ -8,9 +8,15 @@ namespace DataStructuresAndAlgorithms
 {
     public static class Arrays
     {
+        /// <summary>
+        /// inputs an array of integers and the targetSum
+        /// outputs integers that sums up the target
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="targetSum"></param>
+        /// <returns></returns>
         public static int[] TwoNumberSum(int[] array, int targetSum)
         {
-
             int[] result = new int[2];
             HashSet<int> h = new HashSet<int>();
 
@@ -25,6 +31,34 @@ namespace DataStructuresAndAlgorithms
             }
 
             return Array.Empty<int>();
+        }
+
+        public static int[] SortedSquaredArray(int[] array)
+        {
+            int[] result = new int[array.Length];
+            if (array == null || array.Length == 0) return new int[] { };
+            int left = 0;
+            int right = array.Length - 1;
+            int pos = array.Length - 1;
+
+            while (left <= right)
+            {
+                int leftSquare = array[left] * array[left];
+                int rightSquare = array[right] * array[right];
+                if (Math.Abs(array[left]) > Math.Abs(array[right]))
+                {
+                    result[pos] = leftSquare;
+                    left++;
+                }
+                else
+                {
+                    result[pos] = rightSquare;
+                    right--;
+                }
+                pos--;
+            }
+
+            return result;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,11 +5,17 @@ using DataStructuresAndAlgorithms;
  TWO NUMBER SUM
     Given an array of integers, find the numbers that sum up for a given target.
  
-int[] input = new[] { 4, 6, 1, -3 };
+int[] input = new int[] { 4, 6, 1, -3 };
 int target = 3;
 var result  = Arrays.TwoNumberSum(input, target);
 Console.WriteLine($"[{string.Join(", ", result)}]");
+*/
 
- 
+/*
+ SORTED SQUARRED ARRAY
+    Given an array of integers, return the squares of the integers in a sorted order
+int[] input = new int[] { 1,2,3,5,6,8,9};
+var result = Arrays.SortedSquaredArray(input);
+Console.WriteLine($"[{string.Join(", ", result)}]");
 */
 
