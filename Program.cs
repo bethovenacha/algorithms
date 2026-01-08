0ced366294742da338c668891b7492245caa5c1b
BethovenAcha <bmacha2019@gmail.com>
2026-01-08 09:02:00 +0800

    commit the comment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 ﻿
 using DataStructuresAndAlgorithms;
 
+/*
+ TWO NUMBER SUM
+    Given an array of integers, find the numbers that sum up for a given target.
+ 
 int[] input = new[] { 4, 6, 1, -3 };
 int target = 3;
 var result  = Arrays.TwoNumberSum(input, target);
 Console.WriteLine($"[{string.Join(", ", result)}]");
+
+ 
+*/
 
